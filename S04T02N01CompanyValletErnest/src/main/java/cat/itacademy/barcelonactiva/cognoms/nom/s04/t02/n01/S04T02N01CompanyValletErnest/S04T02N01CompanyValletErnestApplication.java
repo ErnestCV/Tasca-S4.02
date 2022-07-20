d5a975dd3339d79c8286400a1b715a0c79001c8b
ErnestCV <107429970+ErnestCV@users.noreply.github.com>
2022-07-20 12:40:52 +0200

    Add files via upload

---
+++ S04T02N01CompanyValletErnest/src/main/java/cat/itacademy/barcelonactiva/cognoms/nom/s04/t02/n01/S04T02N01CompanyValletErnest/S04T02N01CompanyValletErnestApplication.java
@@ -0,0 +1,12 @@
+package cat.itacademy.barcelonactiva.cognoms.nom.s04.t02.n01.S04T02N01CompanyValletErnest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S04T02N01CompanyValletErnestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S04T02N01CompanyValletErnestApplication.class, args);
+	}
+}
