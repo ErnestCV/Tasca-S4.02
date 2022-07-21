b4cb386f0cdbfd2f215d4bb4e5828b204e8e62eb
ErnestCV <107429970+ErnestCV@users.noreply.github.com>
2022-07-21 21:00:49 +0200

    Add files via upload

---
+++ S04T02N03CompanyValletErnest/src/main/java/cat/itacademy/barcelonactiva/CompanyVallet/Ernest/s04/t02/n03/S04T02N03CompanyValletErnest/S04T02N03CompanyValletErnestApplication.java
@@ -0,0 +1,13 @@
+package cat.itacademy.barcelonactiva.CompanyVallet.Ernest.s04.t02.n03.S04T02N03CompanyValletErnest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S04T02N03CompanyValletErnestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S04T02N03CompanyValletErnestApplication.class, args);
+	}
+
+}
---
+++ S04T02N03CompanyValletErnest/src/test/java/cat/itacademy/barcelonactiva/CompanyVallet/Ernest/s04/t02/n03/S04T02N03CompanyValletErnest/S04T02N03CompanyValletErnestApplicationTests.java
@@ -0,0 +1,13 @@
+package cat.itacademy.barcelonactiva.CompanyVallet.Ernest.s04.t02.n03.S04T02N03CompanyValletErnest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S04T02N03CompanyValletErnestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
